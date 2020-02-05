f053ede3981cb3233322034a1868cef1c98f7246
Felipe Canales <felipe.canales.213@gmail.com>
2020-02-05 15:40:31 -0300

    First commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'trz':
+        print(message.author)
+        await message.channel.send("Received!")
+
+client.run(token)
