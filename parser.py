5726fd37f1ccb46115709cc213112dfac70ad604
Felipe Canales <felipe.canales.213@gmail.com>
2020-02-06 10:42:24 -0300

    parsing mentions

--- parser.py
+++ parser.py
@@ -1,16 +1,28 @@
+import re
+
 def parse_msg(msg):
     print("-> Received {} message as command".format(msg))
     parts = msg.split(' ')
     if len(parts) == 1:
         return "help", ""
     if parts[1] == "pu":
-        if len(parts) <= 3 or parts[2] not in ("check", "add", "remove"):
+        try:
+            if len(parts) > 3 and parts[2] in ("check", "add", "remove"):
+                return parts[1] + parts[2], parse_mentions(parts[3:])
+        except ValueError:
             return "help", ""
-        else:
-            return parts[1] + parts[2], " ".join(parts[3:])
     if parts[1] == "server":
         if len(parts) != 3 or parts[2] not in ("status", "start", "stop"):
             return "srverror", ""
         else:
             return "svr" + parts[2], ""
     return parts[1], " ".join(parts[2:])
+
+pat = r"<@\d+>"
+def parse_mentions(parts):
+    ids = []
+    for p in parts:
+        if re.match(pat, p):
+            raise ValueError
+        ids.append(p[2:-1])
+    return ids
